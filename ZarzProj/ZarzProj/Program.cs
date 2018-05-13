970af11ab040de750e4c88f24c9f51d9714d1955
Szymon Kolasa <szymonkolasa@hotmail.com>
2018-05-13 14:10:00 +0200

    Updated Program.cs

--- ZarzProj/ZarzProj/Program.cs
+++ ZarzProj/ZarzProj/Program.cs
@@ -6,7 +6,7 @@ namespace ZarzProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
